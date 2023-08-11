0be4972f10103fd277bd93293c7d92aa24b75f28
Evellyn Rodrigues <evellynrocha489@gmail.com>
2023-08-11 14:27:10 -0300

    questão nível fácil

---
+++ 3422-valor_aproximado_de_pi.py
@@ -0,0 +1,31 @@
+termos = int(input())
+
+s = 0
+qtdTermos = 0
+
+if termos == 0:
+    print(f'{0:.5f}')
+
+elif 1 <= termos < 5:
+        for i in range(1, termos+1, 4):
+            s += 1/(i)**3
+            qtdTermos += 1
+
+            if qtdTermos < termos:
+                s -= 1/(i+2)**3
+                qtdTermos += 1
+        pi = (s*32)**(1/3)
+        print(f'{pi:.5f}')
+
+else:
+
+    for i in range(1, termos+8, 4):
+        s += 1/(i)**3
+        qtdTermos += 1
+
+        if qtdTermos < termos:
+            s -= 1/(i+2)**3
+            qtdTermos += 1
+        
+    pi = (s*32)**(1/3)
+    print(f'{pi:.5f}')
